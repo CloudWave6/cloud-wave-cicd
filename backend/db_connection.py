99990e24957f0d16df57d407c95b4d63922a43c3
Howon Jeong <howon2k@me.com>
2025-07-24 08:40:52 +0000

    update login function

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host='localhost',
+        host='lab-edu-rds-aurora.cluster-cr8yyiy2wzm2.ap-northeast-2.rds.amazonaws.com',
         database='trip_advisor',
         user='user',
-        password='qwer1234'
+        password='qwer1243'
     )
     return conn
